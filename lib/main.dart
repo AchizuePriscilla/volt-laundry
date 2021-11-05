af5dd546cb7deaf33bc3f0b67a3e413936b9bfb5
Achizue Priscilla <priscillaachizue@gmail.com>
2021-11-05 15:14:21 +0100

    Chore: Adds MultiProvider and ScrenUtil for dynamic sizing

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:provider/provider.dart';
+import 'package:volt/utils/providers.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const VoltApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class VoltApp extends StatelessWidget {
+  const VoltApp({Key? key}) : super(key: key);
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+    return ScreenUtilInit(
+        designSize: const Size(360, 630),
+        builder: () => MultiProvider(
+              providers: AppProviders.providers,
+              child: const MaterialApp(
+                  debugShowCheckedModeBanner: false,
+                  home: MyHomePage(
+                    title: 'Volt',
+                  )),
+            ));
   }
 }
 
---
+++ lib/utils/providers.dart
@@ -0,0 +1,11 @@
+import 'package:provider/provider.dart';
+import 'package:provider/single_child_widget.dart';
+
+class AppProviders {
+  //The providers for dependency injection and state management will be added here
+  //as the app will use MultiProvider
+
+  static final providers = <SingleChildWidget>[
+    // ChangeNotifierProvider(create: (_) => LoginViewModel()),
+  ];
+}
