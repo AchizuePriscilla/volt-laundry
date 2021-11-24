e4273cab494abac3ed43a176186bd0101350a7c2
Achizue Priscilla <priscillaachizue@gmail.com>
2021-11-24 01:20:12 +0100

    Feat: Cart Item Container refactor

--- lib/presentation/shared/cart_item_container.dart
+++ lib/presentation/shared/cart_item_container.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:provider/provider.dart';
 import 'package:volt/presentation/shared/shared.dart';
 import 'package:volt/presentation/viewmodels/viewmodels.dart';
@@ -24,15 +25,13 @@ class _CartItemContainerState extends State<CartItemContainer> {
   Widget build(BuildContext context) {
     var laundryDetailsVM = context.read<LaundryVM>();
     return Container(
-      // height: 60.h,
-      // width: MediaQuery.of(context).size.width * .8,
       margin: EdgeInsets.symmetric(vertical: 10.h),
       constraints: BoxConstraints.tight(Size(
         MediaQuery.of(context).size.width * .8,
         60.h,
       )),
       decoration: BoxDecoration(
-          color: Theme.of(context).primaryColorLight,
+          color: const Color(0xffF5F5F8),
           borderRadius: BorderRadius.circular(30.w),
           boxShadow: [
             BoxShadow(
@@ -42,102 +41,152 @@ class _CartItemContainerState extends State<CartItemContainer> {
               offset: const Offset(0, 15),
             )
           ]),
-      child: Container(
-        padding: EdgeInsets.symmetric(horizontal: 25.w),
-        child: Row(
-          mainAxisSize: MainAxisSize.max,
-          children: [
-            SizedBox(
-                height: 27.h,
-                child: Image.asset(
-                    'assets/images/${laundryDetailsVM.getImagePath(widget.clothType)}.png')),
-            const CustomSpacer(
-              flex: 2,
-              horizontal: true,
-            ),
-            Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  laundryDetailsVM.getDesc(widget.clothType),
-                  textAlign: TextAlign.left,
-                  style: GoogleFonts.lato(
-                      fontSize: 15.sp, fontWeight: FontWeight.w700),
-                ),
-                Row(
-                  children: [
-                    Text(
-                      '₦300  ',
-                      style: GoogleFonts.roboto(
-                          fontSize: 13.sp,
-                          fontWeight: FontWeight.w400,
-                          color: Theme.of(context).primaryColor),
-                    ),
-                    DropdownButtonHideUnderline(
-                      child: DropdownButton<String>(
-                        value: selectedValue,
-                        isDense: true,
-                        onChanged: (String? value) {
-                          setState(() {
-                            selectedValue = value!;
-                          });
-                        },
-                        icon: Icon(
-                          Icons.keyboard_arrow_down,
-                          color: Theme.of(context).disabledColor,
-                          // size: 24.w,
-                        ),
-                        style: GoogleFonts.poppins(
-                            fontSize: 12.sp,
-                            fontWeight: FontWeight.w500,
-                            color: Theme.of(context).disabledColor),
-                        items: [
-                          'Men',
-                          'Women',
-                        ].map((String value) {
-                          return DropdownMenuItem(
-                            value: value,
-                            child: Text(value),
-                          );
-                        }).toList(),
-                      ),
-                    ),
-                  ],
-                )
-              ],
-            ),
-            const Expanded(child: SizedBox()),
-            Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Container(
-                  height: 22.h,
-                  width: 47.h,
-                  decoration: BoxDecoration(
-                    color: Theme.of(context).primaryColor,
-                    borderRadius: BorderRadius.circular(10.w),
+      child: Slidable(
+        endActionPane: ActionPane(
+            motion: const ScrollMotion(),
+            extentRatio: .3,
+            children: [
+              Container(
+                  height: 30.h,
+                  width: 30.h,
+                  decoration: const BoxDecoration(
+                      color: Colors.red, shape: BoxShape.circle),
+                  child: Icon(Icons.close,
+                      color: Theme.of(context).primaryColorLight, size: 20.h))
+            ]),
+        child: Container(
+          padding: EdgeInsets.symmetric(horizontal: 25.w),
+          child: Row(
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              SizedBox(
+                  height: 27.h,
+                  child: Image.asset(
+                      'assets/images/${laundryDetailsVM.getImagePath(widget.clothType)}.png')),
+              const CustomSpacer(
+                flex: 2,
+                horizontal: true,
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    laundryDetailsVM.getDesc(widget.clothType),
+                    textAlign: TextAlign.left,
+                    style: GoogleFonts.lato(
+                        fontSize: 15.sp, fontWeight: FontWeight.w700),
                   ),
-                  child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        Icon(
-                          Icons.remove,
-                          size: 15.w,
-                          color: Theme.of(context).primaryColorLight,
+                  Row(
+                    children: [
+                      Text(
+                        '₦300  ',
+                        style: GoogleFonts.roboto(
+                            fontSize: 13.sp,
+                            fontWeight: FontWeight.w400,
+                            color: Theme.of(context).primaryColor),
+                      ),
+                      DropdownButtonHideUnderline(
+                        child: DropdownButton<String>(
+                          value: selectedValue,
+                          isDense: true,
+                          onChanged: (String? value) {
+                            setState(() {
+                              selectedValue = value!;
+                            });
+                          },
+                          icon: Icon(
+                            Icons.keyboard_arrow_down,
+                            color: Theme.of(context).disabledColor,
+                            // size: 24.w,
+                          ),
+                          style: GoogleFonts.poppins(
+                              fontSize: 12.sp,
+                              fontWeight: FontWeight.w500,
+                              color: Theme.of(context).disabledColor),
+                          items: [
+                            'Men',
+                            'Women',
+                          ].map((String value) {
+                            return DropdownMenuItem(
+                              value: value,
+                              child: Text(value),
+                            );
+                          }).toList(),
                         ),
-                        const Text('4'),
-                        Icon(
-                          Icons.add,
-                          size: 15.w,
-                          color: Theme.of(context).primaryColorLight,
-                        ),
-                      ]),
-                )
-              ],
-            )
-          ],
+                      ),
+                    ],
+                  )
+                ],
+              ),
+              const Expanded(child: SizedBox()),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    height: 22.h,
+                    width: 47.h,
+                    decoration: BoxDecoration(
+                      color: Theme.of(context).primaryColor,
+                      borderRadius: BorderRadius.circular(10.w),
+                    ),
+                    child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          Icon(
+                            Icons.remove,
+                            size: 15.w,
+                            color: Theme.of(context).primaryColorLight,
+                          ),
+                          Text('4',
+                              style: TextStyle(
+                                color: Theme.of(context).primaryColorLight,
+                              )),
+                          Icon(
+                            Icons.add,
+                            size: 15.w,
+                            color: Theme.of(context).primaryColorLight,
+                          ),
+                        ]),
+                  ),
+                  SizedBox(
+                      height: 22.h,
+                      width: 47.h,
+                      // color: Colors.black,
+                      child: ListView(
+                        scrollDirection: Axis.horizontal,
+                        children: [
+                          Container(
+                            height: 21.h,
+                            width: 16.h,
+                            decoration: const BoxDecoration(
+                              shape: BoxShape.circle,
+                              color: Colors.red,
+                            ),
+                          ),
+                          Container(
+                            height: 21.h,
+                            width: 16.h,
+                            decoration: const BoxDecoration(
+                              shape: BoxShape.circle,
+                              color: Colors.yellow,
+                            ),
+                          ),
+                          Container(
+                            height: 21.h,
+                            width: 16.h,
+                            decoration: const BoxDecoration(
+                              shape: BoxShape.circle,
+                              color: Colors.green,
+                            ),
+                          )
+                        ],
+                      ))
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
--- lib/presentation/views/cart_view.dart
+++ lib/presentation/views/cart_view.dart
@@ -12,18 +12,37 @@ class CartView extends StatefulWidget {
 class _CartViewState extends State<CartView> {
   @override
   Widget build(BuildContext context) {
-    return ResponsiveWidget(builder: (_, size) {
-      return Container(
-        margin: EdgeInsets.symmetric(horizontal: 20.w),
-        child: ListView(
-          children: [
-            CartItemContainer(
-              clothType: ClothType.tShirt,
-              onTap: () {},
-            ),
-          ],
+    return ResponsiveWidget(
+        appBar: CustomAppBar(
+          text: 'Cart',
         ),
-      );
-    });
+        builder: (_, size) {
+          return Container(
+            height: size.height,
+            width: size.width,
+            color: const Color(0xffF5F5F8),
+            padding: EdgeInsets.symmetric(horizontal: 20.w),
+            child: ListView(
+              children: [
+                CartItemContainer(
+                  clothType: ClothType.tShirt,
+                  onTap: () {},
+                ),
+                CartItemContainer(
+                  clothType: ClothType.shorts,
+                  onTap: () {},
+                ),
+                CartItemContainer(
+                  clothType: ClothType.dresses,
+                  onTap: () {},
+                ),
+                CartItemContainer(
+                  clothType: ClothType.others,
+                  onTap: () {},
+                ),
+              ],
+            ),
+          );
+        });
   }
 }
