1d87bf6651d253f9b94162ed5d92609d6bab4759
Priscilla Achizue <priscillaachizue@gmail.com>
2022-02-10 08:13:34 +0100

    Removes cached page check from splash screen logic

--- lib/presentation/viewmodels/splash_screen_view_model.dart
+++ lib/presentation/viewmodels/splash_screen_view_model.dart
@@ -2,29 +2,6 @@ import 'package:volt/utils/utils.dart';
 import 'base_view_model.dart';
 
 class SplashScreenVM extends BaseViewModel {
-  // /Checks if there is any cached page and navigates to it
-  bool _checkLastPage() {
-    bool isCached = false;
-    String? lastpage = localCache.getFromLocalCache(lastPage) as String?;
-
-    if (lastpage == verificationViewRoute) {
-      isCached = true;
-      navigationHandler.pushReplacementNamed(
-        verificationViewRoute,
-      );
-    }
-    if (lastpage == setProfilePictureViewRoute) {
-      isCached = true;
-      navigationHandler.pushReplacementNamed(setProfilePictureViewRoute);
-    }
-    return isCached;
-  }
-
-  // Future<void> handleNavigation() async {
-  //   await Future.delayed(const Duration(seconds: 2)).then((value) async {
-  //     navigationHandler.pushNamed(onboardingViewRoute);
-  //   });
-  // }
 
   ///Handles navigation from SplashScreenView on app start
   Future<void> handleNavigation() async {
@@ -42,13 +19,7 @@ class SplashScreenVM extends BaseViewModel {
                 localCache.getFromLocalCache(onboardingDone) != null;
 
             if (hasBeenOnboarded) {
-              //if user has been onboarded, check if there is any cached page
-              //and navigate to it, if any
-              if (_checkLastPage()) {
-                return;
-              }
-
-              //no cached page but user has been onboarded?
+              //User has been onboarded?
               //navigate to LoginView
 
               navigationHandler.pushReplacementNamed(logInViewRoute);
